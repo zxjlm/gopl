ccb55b366d9cab31f401faf37919f9bafe1b3ea2
harumonia <zxjlm233@gmail.com>
2022-04-02 16:06:15 +0800

    chore: change name

--- ch1/5_GreenLissajous.go
+++ ch1/5_GreenLissajous.go
@@ -1,5 +1,8 @@
 // 修改前面的Lissajous程序里的调色板，由黑色改为绿色。
 // 我们可以用color.RGBA{0xRR, 0xGG, 0xBB, 0xff}来得到#RRGGBB这个色值，三个十六进制的字符串分别代表红、绿、蓝像素。
+
+// exec cmd in terminal: go run 5_ColorfulLissajous.go > out_put.gif
+
 package main
 
 import (
---
+++ ch1/6_ColorfulLissajous.go
@@ -0,0 +1,2 @@
+// 修改Lissajous程序，修改其调色板来生成更丰富的颜色，然后修改SetColorIndex的第三个参数，看看显示结果吧。
+package main
