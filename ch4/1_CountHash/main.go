13649d0ac848d66af5430c4b9cd546187d3e8b7b
harumonia <zxjlm233@gmail.com>
2022-04-10 22:54:57 +0800

    ex: 4.1

---
+++ ch4/1_CountHash/main.go
@@ -0,0 +1,30 @@
+// 编写一个函数，计算两个SHA256哈希码中不同bit的数目。（参考2.6.2节的PopCount函数。)
+
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func countHash(c1, c2 [32]byte) {
+	var count int
+	for i := 0; i < len(c1); i++ {
+		if c1[i] != c2[i] {
+			count++
+		}
+	}
+	fmt.Printf("%d\n", count)
+}
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+	fmt.Printf("%x\n%x\n", c1, c2)
+	countHash(c1, c2)
+	// Output:
+	// 2d711642b726b04401627ca9fbac32f5c8530fb1903cc4db02258717921a4881
+	// 4b68ab3847feda7d6c62c1fbcbeebfa35eab7351ed5e78f4ddadea5df64b8015
+	// false
+	// [32]uint8
+}
